c4f4182b79c88443362d09770145feda68ce98af
MattiaMessana <mattia.messana@icloud.com>
2024-06-28 18:57:20 +0200

    feat: create ProjectCard component

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,11 +1,14 @@
 <script>
 import axios from 'axios';
+import ProjectCard from "./ProjectCard.vue";
 
 export default {
+components: {
+    ProjectCard,
+},
  data() {
     return {
         projects: [],
-        imgUrlBase: 'http://127.0.0.1:8000/storage',
     }
  },
  created() {
@@ -18,22 +21,11 @@ export default {
 </script>
 
 <template>
+
 <div class="container my-2">
     <div class="row row-cols-4">
         <div class="col my-2" v-for="project in projects">
-            <div class="card h-100">
-                <img class="card-img-top" 
-                 :src="project.cover_img ? `${imgUrlBase}/${project.cover_img}` : `https://placehold.co/500x300?text=immagine+non+disponibile`" 
-                 :alt="project.title">
-                <div class="card-body ">
-                    <h5 class="card-title">{{ project.title }}</h5>
-                    <p class="card-text">{{ project.slug }}</p>
-                    <p class="card-text">{{ project.category ? project.category.name : `nessuna categoria` }}</p>
-                    <div>
-                        <span class="badge text-bg-primary mx-1" v-for="tech in project.technologies">{{ tech.name }}</span>
-                    </div>
-                </div>
-            </div>
+            <ProjectCard :project="project" />
         </div>
     </div>
 </div>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+        props: {
+            project: Object,
+        },    
+        data() {
+            return {
+                imgUrlBase: 'http://127.0.0.1:8000/storage',
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="card h-100">
+        <img class="card-img-top" 
+            :src="project.cover_img ? `${imgUrlBase}/${project.cover_img}` : `https://placehold.co/500x300?text=immagine+non+disponibile`" 
+            :alt="project.title">
+        <div class="card-body ">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">{{ project.slug }}</p>
+            <p class="card-text">{{ project.category ? project.category.name : `nessuna categoria` }}</p>
+            <div>
+            <span class="badge text-bg-primary mx-1" v-for="tech in project.technologies">{{ tech.name }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
