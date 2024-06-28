9afffa098cd65ca48a268a69f29c810e07250101
MattiaMessana <mattia.messana@icloud.com>
2024-06-28 17:21:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import MyMain from "./components/MyMain.vue";
+
+  export default {
+    components: {
+      MyMain,
+    },
+
+  };
+</script>
+
+<template>
+
+  <MyMain />
+  
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from 'axios';
+
+export default {
+ data() {
+    return {
+        projects: []
+    }
+ },
+ created() {
+    axios.get('http://127.0.0.1:8000/api/project').then((resp) => {
+        // console.log(resp.data.result);
+        this.projects = resp.data.result;
+    });
+ },
+}
+</script>
+
+<template>
+<div class="container my-2">
+    <div class="row row-cols-4">
+        <div class="col my-2" v-for="project in projects">
+            <div class="card h-100">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body ">
+                    <h5 class="card-title">{{ project.title }}</h5>
+                    <p class="card-text">{{ project.description }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
